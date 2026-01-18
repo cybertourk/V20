ad7323d214e3ff5dd5e58654ed9663499a99a282
cybertourk <cybertourk@gmail.com>
2026-01-18 12:59:30 -0600

    Update necromancy-rituals.js

--- necromancy-rituals.js
+++ necromancy-rituals.js
@@ -1,1 +1,23 @@
+/**
+ * V20 NECROMANCY RITUALS DATA
+ * Stores detailed information for Necromancy Rituals by Level.
+ * Separated from necromancy-data.js to reduce file size.
+ */
 
+export const NECROMANCY_RITUALS = {
+    1: {
+        // Level 1 Rituals
+    },
+    2: {
+        // Level 2 Rituals
+    },
+    3: {
+        // Level 3 Rituals
+    },
+    4: {
+        // Level 4 Rituals
+    },
+    5: {
+        // Level 5 Rituals
+    }
+};
