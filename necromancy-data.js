684cc57e6cd312e0c144eada9357ce511fdb952a
cybertourk <cybertourk@gmail.com>
2026-01-18 12:55:39 -0600

    Update necromancy-data.js

--- necromancy-data.js
+++ necromancy-data.js
@@ -1,1 +1,9 @@
+/**
+ * V20 NECROMANCY DATA
+ * Stores detailed information for Necromancy Paths and Rituals.
+ * Separated from disciplines-data.js for modularity.
+ */
 
+export const NECROMANCY_DATA = {
+    // Paths will be added here
+};
