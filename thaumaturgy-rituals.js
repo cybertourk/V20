ddcb8cd4f4b0a1b6ddb1a69b39239dfeed21d976
cybertourk <cybertourk@gmail.com>
2026-01-18 12:54:21 -0600

    Create thaumaturgy-rituals.js

---
+++ thaumaturgy-rituals.js
@@ -0,0 +1,30 @@
+/**
+ * V20 THAUMATURGY RITUALS DATA
+ * Stores detailed information for Thaumaturgy Rituals by Level.
+ * Separated from thaumaturgy-data.js to reduce file size.
+ */
+
+export const THAUMATURGY_RITUALS = {
+    1: {
+        // Level 1 Rituals
+        // Example format:
+        // "Ritual Name": {
+        //     name: "Ritual Name",
+        //     desc: "Description...",
+        //     system: "System...",
+        //     roll: { ... }
+        // }
+    },
+    2: {
+        // Level 2 Rituals
+    },
+    3: {
+        // Level 3 Rituals
+    },
+    4: {
+        // Level 4 Rituals
+    },
+    5: {
+        // Level 5 Rituals
+    }
+};
